815c2878f889191ea76843fdd0ae2113308ee006
Priyanshu Dhawan <priyanshudhawan55@gmail.com>
2024-01-04 21:56:09 +0530

    layout of header compo

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,17 @@
 import { forwardRef } from "react"
+import { twMerge } from "tailwind-merge"
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-}
-
-const Button = forwardRef<HTMLButtonElement, ButtonProps>
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
+    className, children, disabled, type = 'button',...props
+}, ref) => {
+    return (
+        <button type={type} ref={ref} disabled={disabled}  className={twMerge(`w-full rounded-full bg-green-500 border border-transparent px-3 py-3 disabled:cursor-not-allowed disabled:opacity-40 text-black font-bold hover:opacity-75 transition`, className)}>
+             {children}
+        </button>
+    )
+})
 
 Button.displayName = 'Button'
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -44,11 +44,15 @@ const Header : React.FC<HeaderProps> = ({
             <div className="flex justify-between items-center gap-x-4">
                   <>
                   <div>
-                    <Button>Sign Up</Button>
+                    <Button onClick={() => {}} className="bg-transparent text-neutral-300 font-medium">Sign Up</Button>
+                  </div>
+                  <div>
+                    <Button onClick={() => {}} className="bg-white px-6 py-2">Log in</Button>
                   </div>
                   </>
             </div>
         </div>
+        {children}
     </div>
   )
 }
